df0103b8dba4079755365468ac26edeac9104228
Hsu Htet <hsu.htet.aung@digitallaboratory.net>
2019-05-10 13:23:22 +0630

    #card-21316290 - fix testing

--- 4-30.php
+++ 4-30.php
@@ -4,7 +4,7 @@ $prices['lunch']['Cashew Nuts and White Mushrooms']=4;
 $prices['dinner']['Braised Bamboo Fungus']=8.95;
 $prices['dinner']['total']=$prices['dinner']['Sweet Corn and Asparagus']+
 							$prices['dinner']['Braised Bamboo Fungus'];					
-$prices[0][0]='Chestnut Bun';
+$prices[0][0]='Chhestnut Bun';
 $prices[0][1]='Walnut Bun';
 $prices[0][2]='Peanut Bun';
 $prices[1][0]='Chestnut Salad';
